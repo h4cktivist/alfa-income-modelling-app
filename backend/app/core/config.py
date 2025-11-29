ec35c5dad856e6852af1c0c820432af8c2752f88
h4cktivist <popovvadim0605@gmail.com>
2025-11-29 18:52:48 +0500

    fix: cors fix

--- backend/app/core/config.py
+++ backend/app/core/config.py
@@ -6,10 +6,10 @@ class Settings(BaseSettings):
     DATABASE_URL: str = "postgresql+asyncpg://postgres:postgres@localhost:5432/alfa_hack"
     CORS_ORIGINS: list[str] = [
         "http://localhost:3000",
-        "http://localhost:5173",
+        "http://localhost:5174",
         "http://localhost:8080",
         "http://127.0.0.1:3000",
-        "http://127.0.0.1:5173",
+        "http://127.0.0.1:5174",
         "http://127.0.0.1:8080",
     ]
 
